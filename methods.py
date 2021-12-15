51888523b96c0a29268bbe61b836d1b9755b78da
Tianfeng LYU <tflyu1998@gmail.com>
2021-12-15 17:02:07 +0100

    models

--- methods.py
+++ methods.py
@@ -4,6 +4,10 @@ import math
 import numpy
 import random
 import pandas as pd
+from sklearn import svm
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 def sigmoid(x,theta):
     z = 0
@@ -11,7 +15,7 @@ def sigmoid(x,theta):
         z = z + x[i]*theta[i]
     sum = 1/(1.0+math.exp(-z))
     return sum
-    
+
 def derivative(X,Y,theta,size,j): # j represents dimension
     sum = 0.0
     for i in range(size):
@@ -24,7 +28,7 @@ def loss(X,Y,theta,size):
         h = sigmoid(X[i],theta)
         sum = sum + (h-Y[i])*(h-Y[i])
     return (sum/2*size)
-            
+
 def gradient(X,Y,theta,size,rate,count):
     for i in range(count):
         t = []
@@ -33,7 +37,7 @@ def gradient(X,Y,theta,size,rate,count):
             t.append(h)
         for j in range(4):
             theta[j] = t[j]
-    
+
 def logistic_regression(data_path, num_gradient_descent=30000, rate=0.002):
     data = pd.read_csv(data_path,header=None)
     X = data[[0,1,2,3]] # the first four values are eigenvalues
@@ -41,10 +45,10 @@ def logistic_regression(data_path, num_gradient_descent=30000, rate=0.002):
     size = data.shape[0]
     theta = numpy.zeros((4)) # 4 represents the number of individuals instead of the index (which starts from 0)
     gradient(X.values.tolist(),Y[4].tolist(),theta,size,rate,num_gradient_descent)
-      
+
     X, Y = X.values.tolist(), Y.values.tolist()
     b = 0
-    for i in range(1000): 
+    for i in range(1000):
         z = 0
         j = random.randint(0,1371)
         for k in range (4):
@@ -56,3 +60,27 @@ def logistic_regression(data_path, num_gradient_descent=30000, rate=0.002):
         elif Y[j][0] == 0:
             b = b + 1
     return b/1000
+
+
+def Svm_acc(X_train, X_test, y_train, y_test):
+    model = svm.SVC(kernel='linear', gamma='scale')
+    model.fit(X_train,y_train)
+    y_pred_svm = model.predict(X_test)
+    svm_accuracy = accuracy_score(y_pred_svm,y_test)
+    return svm_accuracy
+
+
+def DecisionTree_acc(X_train, X_test, y_train, y_test):
+    model = DecisionTreeClassifier(criterion='entropy',spliltter='best',random_state=0)
+    model.fit(X_train,y_train)
+    y_pred_dt = model.predict(X_test)
+    dt_accuracy = accuracy_score(y_pred_dt,y_test)
+    return dt_accuracy
+
+
+def Kmeans_acc(X_train, X_test, y_train, y_test):
+    model = KMeans(n_clusters=2, random_state=0)
+    model.fit(X_train,y_train)
+    y_pred_km = model.predict(X_test)
+    km_accuracy = accuracy_score(y_pred_km,y_test)
+    return km_accuracy
